fe9bdb3c905fffcc2a7cb49711a2ce114d8658d3
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2023-07-18 19:43:20 +0545

    Created all the wanted component added formSlices

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import CarForm from './Components/CarForm';
+import CarList from './Components/CarList';
+import CarSearch from './Components/CarSearch';
+import CarValue from './Components/CarValue';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <CarForm />
+      <CarList />
+      <CarSearch />
+      <CarValue />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/store/slices/formSlices.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const formSlices = createSlice({
+  name: 'form',
+  initialState: {
+    name: '',
+    cost: 0,
+  },
+  reducers: {
+    changeName(state, action) {
+      state.name = action.payload;
+    },
+    changeCost(state, action) {
+      state.cost = action.payload;
+    },
+  },
+});
+
+export const { changeCost, changeName } = createSlice.actions;
+export const formReducer = createSlice.reducer;
